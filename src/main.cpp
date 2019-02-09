68a13723e7a163ad82694bfde7cb4a2e119a4a2b
BitSails <BitSails@users.noreply.github.com>
2019-02-08 22:55:43 -0400

    Uncomment last line in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,6 @@ int main()
 
     cin>>seed>>bases>>n;//read the three inputs
 
-//    cout <<"\n\nOutput:\n\t"<<randDNA(seed,bases,n)<<endl; //print output  
+    cout <<"\n\nOutput:\n\t"<<randDNA(seed,bases,n)<<endl; //print output  
 }
 
